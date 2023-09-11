bd5b6c4faf647649624c4aaa11e141338a3df34e
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2023-09-11 10:18:16 +0300

    </>

--- desktopEnvironments/dwm.nix
+++ desktopEnvironments/dwm.nix
@@ -3,7 +3,7 @@
 {
   services.xserver.windowManager.dwm.enable = true;
   environment.systemPackages = with pkgs; [
-    picom-jonaburg
+    picom-next
     st # teminal
     dmenu #application launcher most people use
   ];
