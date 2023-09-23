aa9855f6af23187472c92547389b1ea4e1725800
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2023-09-24 01:21:30 +0300

    add waybar overlay for hyprland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,6 +24,16 @@
     };
   };
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      waybar = prev.waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+        postPatch = (oldAttrs.postPatch or "") + ''
+          sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp'';
+      });
+    })
+  ];
+
   # Swap
   zramSwap = {
     enable = true;
@@ -167,6 +177,7 @@
     cava
     kitty
     wofi
+    waybar
     jq
     bobcat
     wlroots
