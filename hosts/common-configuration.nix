9f8c120148a11c584b492c5861a0c7f56f90d66c
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2023-12-25 21:00:20 +0200

    Fixing a bug!

--- hosts/common-configuration.nix
+++ hosts/common-configuration.nix
@@ -12,7 +12,10 @@
     inputs.home-manager.nixosModules.home-manager
   ];
 
-  stylix.image = ./../resources/wallpapers/48.png;
+  stylix.image = pkgs.fetchurl {
+    url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+    sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+  };
   stylix.polarity = "dark";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
@@ -261,13 +264,6 @@
     obs-studio
     rnote
     xournalpp
-    xclip
-    xorg.libX11
-    xorg.libxcb
-    xorg.libXft
-    xorg.xinit
-    xorg.xinput
-    xorg.libXinerama
     nix-prefetch-git
     nix-prefetch-github
     udiskie
