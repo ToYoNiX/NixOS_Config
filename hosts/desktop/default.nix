41e37169398900c6151bfb8eff9c4b41bbe1af46
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-01-04 16:18:08 +0200

    Adding trilium server to my desktop.

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -13,4 +13,20 @@
     # Enable Trilium server
     ./../common/trilium-server.nix
   ];
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."myNotes.com" = {
+      locations."/trilium/" = {
+        proxyPass = "http://127.0.0.1:8080/";
+        extraConfig = ''
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection 'upgrade';
+          proxy_set_header Host $host;
+          proxy_cache_bypass $http_upgrade;
+        '';
+      };
+    };
+  };
 }
