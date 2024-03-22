f1f1dae10d4132bb4d9bb24f5391e8dec91f5bbb
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2024-03-23 01:04:25 +0200

    Fix for windows dual boot.

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -86,6 +86,7 @@
 
   # Set your time zone.
   time.timeZone = "Africa/Cairo";
+  time.hardwareClockInLocalTime = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
